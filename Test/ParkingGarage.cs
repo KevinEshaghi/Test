5b66bb653d584a1b66a73a2d643d3d177b541182
Kevin <Kevin@LAPTOP-HGP2RO1B>
2024-11-21 10:41:21 +0100

    Add project files.

---
+++ Test/ParkingGarage.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ParkingGarageTests
+{
+    internal class ParkingGarage
+    {
+        public ParkingGarage()
+        {
+        }
+
+        internal void AssignSpot(int spotNumber, string vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetVehicleInSpot(int spotNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsSpotAvailable(int spotNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
